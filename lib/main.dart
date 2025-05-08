e02d31e77ab6db7599b985a11eeae74d9707b732
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2025-05-08 17:36:31 +0300

    init app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/dashboard_view.dart';
+
+void main() {
+  runApp(const ResponsiveDashboard());
+}
+
+class ResponsiveDashboard extends StatelessWidget {
+  const ResponsiveDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DashboardView(),
+    );
+  }
+}
