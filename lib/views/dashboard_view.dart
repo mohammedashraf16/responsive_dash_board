d738c4957cc3f642450018d22966ea5a4ff129f4
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2025-05-09 01:27:54 +0300

    add text styles & images

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/dashboard_desktop_layout.dart';
+import 'package:responsive_dash_board/views/widgets/responsive_layout_widget.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => SizedBox(),
+        desktopLayout: (context) => DashboardDesktopLayout(),
+      ),
+    );
   }
 }
